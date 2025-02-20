0f6236277291f0ffac8be763527f0626b9bfc9a4
jbanes <jbanes@gmail.com>
2025-02-20 16:28:21 -0600

    Translating Create.vue to English

--- src/views/barang/Create.vue
+++ src/views/barang/Create.vue
@@ -6,18 +6,18 @@ import { onBeforeRouteLeave } from "vue-router";
 const barangStore = useBarangStore();
 
 const form = reactive({
-    nama: "",
-    kategori: "",
-    berat: "",
+    name: "",
+    category: "",
+    weight: "",
 });
 
-const namaInput = ref(null);
-nextTick(() => namaInput.value.focus());
+const nameInput = ref(null);
+nextTick(() => nameInput.value.focus());
 
 watch(
-    () => form.kategori,
+    () => form.category,
     (newValue) => {
-        form.kategori = newValue.toUpperCase();
+        form.category = newValue.toUpperCase();
     }
 );
 
@@ -37,41 +37,41 @@ const saveBarang = async () => {
         <div class="mt-10">
             <form @submit.prevent="saveBarang" class="max-w-sm mx-auto">
                 <div class="mb-2">
-                    <label for="nama" class="block text-sm font-medium text-slate-700">Nama</label>
+                    <label for="name" class="block text-sm font-medium text-slate-700">Name</label>
                     <input
                         type="text"
-                        id="nama"
-                        ref="namaInput"
-                        v-model="form.nama"
+                        id="name"
+                        ref="nameInput"
+                        v-model="form.name"
                         class="block w-full px-3 py-2 border-2 rounded-md border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-amber-500 dark:focus:border-amber-600 focus:ring-amber-500 dark:focus:ring-amber-600 text-slate-900 sm:text-sm focus:outline-none"
-                        :class="{ 'border-rose-500': error.nama }"
-                        placeholder="Nama barang"
+                        :class="{ 'border-rose-500': error.name }"
+                        placeholder="Product Name"
                     />
-                    <span v-if="error.nama" class="text-sm text-rose-500">{{ error.nama }}</span>
+                    <span v-if="error.name" class="text-sm text-rose-500">{{ error.name }}</span>
                 </div>
                 <div class="mb-2">
-                    <label for="kategori" class="block text-sm font-medium text-slate-700">Kategori</label>
+                    <label for="category" class="block text-sm font-medium text-slate-700">Category</label>
                     <input
                         type="text"
-                        id="kategori"
-                        v-model="form.kategori"
+                        id="category"
+                        v-model="form.category"
                         class="block w-full px-3 py-2 border-2 rounded-md border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-amber-500 dark:focus:border-amber-600 focus:ring-amber-500 dark:focus:ring-amber-600 text-slate-900 sm:text-sm focus:outline-none"
-                        :class="{ 'border-rose-500': error.kategori }"
-                        placeholder="Kategori"
+                        :class="{ 'border-rose-500': error.category }"
+                        placeholder="Category"
                     />
-                    <span v-if="error.kategori" class="text-sm text-rose-500">{{ error.kategori }}</span>
+                    <span v-if="error.category" class="text-sm text-rose-500">{{ error.category }}</span>
                 </div>
                 <div class="mb-2">
-                    <label for="berat" class="block text-sm font-medium text-slate-700">Berat</label>
+                    <label for="weight" class="block text-sm font-medium text-slate-700">Weight</label>
                     <input
                         type="number"
-                        id="berat"
-                        v-model="form.berat"
+                        id="weight"
+                        v-model="form.weight"
                         class="block w-full px-3 py-2 border-2 rounded-md border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-amber-500 dark:focus:border-amber-600 focus:ring-amber-500 dark:focus:ring-amber-600 text-slate-900 sm:text-sm focus:outline-none"
-                        :class="{ 'border-rose-500': error.berat }"
-                        placeholder="Berat"
+                        :class="{ 'border-rose-500': error.weight }"
+                        placeholder="Weight"
                     />
-                    <span v-if="error.berat" class="text-sm text-rose-500">{{ error.berat }}</span>
+                    <span v-if="error.weight" class="text-sm text-rose-500">{{ error.weight }}</span>
                 </div>
                 <div class="mt-2">
                     <button
@@ -80,7 +80,7 @@ const saveBarang = async () => {
                         class="inline-flex items-center justify-center w-full px-4 py-2 text-sm font-semibold tracking-widest text-center text-black uppercase transition duration-150 ease-in-out border border-transparent rounded-lg shadow-md active:bg-amber-600 bg-amber-400 hover:bg-amber-500 focus:ring-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-slate-800"
                         :class="{ 'cursor-not-allowed opacity-50': barangStore.loading }"
                     >
-                        {{ barangStore.loading ? "Loading..." : "Simpan" }}
+                        {{ barangStore.loading ? "Loading..." : "Save" }}
                     </button>
                 </div>
             </form>
