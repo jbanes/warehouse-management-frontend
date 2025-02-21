21074684b7ab04c0816141baa6d4aefb32182483
jbanes <jbanes@gmail.com>
2025-02-20 21:17:44 -0600

    Centering content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,15 +8,19 @@
                     :to="{ name: 'ship.form', params: { type: 'IN' } }"
                     class="py-6 text-sm font-medium bg-white border-2 rounded-md border-slate-900 text-slate-900 px-14"
                 >
+                    <div style="text-align: center;">
                     <img src="img/001.png" alt="" class="h-14" />
                     Receive goods
+                    </div>
                 </RouterLink>
                 <RouterLink
                     :to="{ name: 'ship.form', params: { type: 'OUT' } }"
                     class="py-6 text-sm font-medium bg-white border-2 rounded-md border-slate-900 text-slate-900 px-14"
                 >
+                    <div style="text-align: center;">
                     <img src="img/002.png" alt="" class="h-14" />
                     Ship goods
+                    </div>
                 </RouterLink>
             </div>
         </div>
